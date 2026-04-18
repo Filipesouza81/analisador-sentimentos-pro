de0bc95d72ef870056f6a4bdeac42f6425cea8fb
Filipesouza81 <filipesouza81@gmail.com>
2026-04-17 23:18:36 -0300

    Feat: Minimizar terminal

--- iniciar.bat
+++ iniciar.bat
@@ -1,5 +1,10 @@
 @echo off
+if not "%minimized%"=="" goto :minimized
+set minimized=true
+start /min cmd /C "%~dp0iniciar.bat"
+goto :EOF
+
+:minimized
 echo Iniciando o Analisador de Sentimentos...
 call .venv\Scripts\activate
 streamlit run app.py
-pause
